b2d08e62c9b858edc7a90f16fdc9681b04be5b85
Cale Gibbard <cgibbard@gmail.com>
2021-05-22 03:53:47 -0400

    Add some autocolouring and automatic reassignment of notes in a regular fashion, functions to look up logical coordinates of a key by its board and key number.

--- src/Lumatone/Config.hs
+++ src/Lumatone/Config.hs
@@ -7,6 +7,8 @@ import Control.Monad
 import Data.Ini
 import Data.Colour
 import Data.Colour.SRGB
+import Data.Colour.RGBSpace
+import Data.Colour.RGBSpace.HSV
 import qualified Data.Map as Map
 import Data.Map (Map)
 import qualified Data.Text as T
@@ -14,6 +16,34 @@ import Data.Text (Text)
 import Data.Maybe
 import qualified Data.HashMap.Strict as HM
 import Data.List
+
+-- | Map from key numbers on a single board to sensible (Int, Int) coordinates, where the most southwest key is (0,0).
+oneBoardCoordMap :: Map Int (Int, Int)
+oneBoardCoordMap = Map.fromList (zip [0..55] (concat rows))
+  where
+    rows = -- Saner coordinates for the rows of a single 56-key Terpstra board.
+      [[(k,8) | k <- [-4 .. -3]] -- OO
+      ,[(k,7) | k <- [-3 .. 1]]  --  OOOOO
+      ,[(k,6) | k <- [-3 .. 2]]  --  OOOOOO
+      ,[(k,5) | k <- [-2 .. 3]]  --   OOOOOO
+      ,[(k,4) | k <- [-2 .. 3]]  --   OOOOOO
+      ,[(k,3) | k <- [-1 .. 4]]  --    OOOOOO
+      ,[(k,2) | k <- [-1 .. 4]]  --    OOOOOO
+      ,[(k,1) | k <- [0 .. 5]]   --     OOOOOO
+      ,[(k,0) | k <- [0 .. 5]]   --     OOOOOO
+      ,[(k,-1) | k <- [2 .. 6]]  --       OOOO
+      ,[(k,-2) | k <- [5 .. 6]]  --         OO
+      ]
+
+-- | Map from board index and key number to sensible 2D coordinates. The most southwest key is (0,0).
+lumatoneCoordMap :: Map Int (Map Int (Int, Int))
+lumatoneCoordMap = Map.fromList [(b, Map.map (\(i,j) -> (7*b+i, (-2)*b+j)) oneBoardCoordMap) | b <- [0..4]]
+
+-- | Unsafely look up a coordinates of a key. Handy when reconstructing layouts and you know the index will be valid by construction.
+lookupCoords :: Int -> Int -> (Int, Int)
+lookupCoords b k = maybe (error ("lookupCoords: bad indices: " ++ show (b,k))) id $ do
+  m <- Map.lookup b lumatoneCoordMap
+  Map.lookup k m
 
 -- | Type for representing the contents of a .ltn configuration. Currently leaves out the non-key configuration.
 data LumatoneConfig = LumatoneConfig
@@ -88,6 +118,9 @@ withKeyConf f (LumatoneConfig m) = LumatoneConfig (Map.map (Map.map f) m)
 withBoardKeyConf :: (Int -> KeyConf -> KeyConf) -> LumatoneConfig -> LumatoneConfig
 withBoardKeyConf f (LumatoneConfig m) = LumatoneConfig (Map.mapWithKey (\b -> Map.map (f b)) m)
 
+withBoardKeyConf' :: (Int -> Int -> KeyConf -> KeyConf) -> LumatoneConfig -> LumatoneConfig
+withBoardKeyConf' f (LumatoneConfig m) = LumatoneConfig (Map.mapWithKey (\b -> Map.mapWithKey (f b)) m)
+
 withChannel :: (Int -> Int) -> LumatoneConfig -> LumatoneConfig
 withChannel f = withKeyConf (\kc -> kc { keyConf_chan = f (keyConf_chan kc - 1) `mod` 16 + 1 })
 
@@ -108,7 +141,10 @@ processLtn' f fp fp' = do
         Right c -> writeIniFileWith lumatoneIniSettings fp' (updateLumatoneConfig (f c) i)
 
 chanToOctave :: (Ord n, Num n) => n -> n
-chanToOctave n = if n > 7 then n - 16 else n
+chanToOctave n = if n > 7 then n - 16 - 1 else n - 1
+
+octaveToChan :: (Ord n, Num n) => n -> n
+octaveToChan n = if n < 0 then n + 16 + 1 else n + 1
 
 gamut :: Int -> (Int, Int) -> KeyConf -> Double
 gamut period (a,b) c = (index - fI a) / (fI (b - a))
@@ -124,3 +160,31 @@ dechannelify n kc =
                    | k >= 128          = rechan (k-n, c+1)
   in let (k,c) = rechan (n * chanToOctave (keyConf_chan kc) + keyConf_key kc, 1)
      in kc { keyConf_chan = c, keyConf_key = k }
+
+-- | With the given period, the given starting pitch (relative to that period) for the southwestern point of the keyboard, and the given pair of step sizes horizontally and right-upward,
+-- remap the notes on the entire keyboard. Does not affect colours.
+
+regularSteps :: Int -> Int -> (Int, Int) -> LumatoneConfig -> LumatoneConfig
+regularSteps period note0 (stepX, stepY) = withBoardKeyConf' keyChanFunction
+  where
+    computeNote b i =
+      let (x,y) = lookupCoords b i
+      in note0 + x * stepX + y * stepY
+    keyChanFunction b i kc =
+      let n = computeNote b i
+          c = octaveToChan (n `div` period)
+          k = n `mod` period
+      in kc { keyConf_chan = c, keyConf_key = k }
+
+
+autoColour :: Int -- ^ Period (hues are assigned equally spaced modulo this period)
+           -> Double -- ^ Saturation of colours [0-1]
+           -> Double -- ^ Value (brightness) of colours [0-1]
+           -> KeyConf -- ^ Key config to transform
+           -> KeyConf
+autoColour period saturation value k = k { keyConf_col = noteToColour period (keyConf_key k) saturation value }
+
+noteToColour :: Int -> Int -> Double -> Double -> Colour Double
+noteToColour period note saturation value = col
+  where hue = 360 * (fromIntegral (note `mod` period) / fromIntegral period)
+        col = uncurryRGB (rgbUsingSpace sRGBSpace) (hsv hue saturation value)
